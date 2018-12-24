1cdd7bd4bb1822297c8878f31499c8dc7814eb99
Eric Gustafson <edg@cs.ucsb.edu>
2018-12-24 00:08:05 +0000

    Rudimentary thingy to convert our CFG to FunctionSimSearch format

---
+++ bdsig/fss.py
@@ -0,0 +1,31 @@
+import angr
+from angr.analyses.cfg import CFGFast
+from networkx.classes import DiGraph
+from angr.knowledge_plugins.functions import Function
+
+
+def tg_to_fss(f: Function, cfg: CFGFast):
+    fg = f.transition_graph
+    disasm = cfg.project.analyses.Disassembly(f)
+    fss = dict()
+    fss['edges'] = list()
+    fss['nodes'] = list()
+    # Convert edges
+    for edg in fg.edges:
+        src, dst = edg
+        fss_edge = {'source': src.addr, 'destination': dst.addr}
+        fss['edges'].append(fss_edge)
+
+    for n in tg.nodes:
+        fss_node = {'address': n.addr}
+        fss_node['instructions'] = list()
+        for iaddr in disasm.block_to_insn_addrs[n.addr]:
+            i = disasm.raw_result_map['instructions'][iaddr]
+            fss_ins = dict()
+            fss_ins['mnemonic'] = i.mnemonic.opcode_string
+            fss_ins['operands'] = [o.render() for o in i.operands]
+            fss_node['instructions'].append(fss_ins)
+        fss['nodes'].append(fss_node)
+    return fss
+
+
